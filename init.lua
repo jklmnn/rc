e817047c0c89e6fd560ce82e2b4ab097adce2fbc
Johannes Kliemann <jk@jkliemann.de>
2024-07-23 13:23:55 +0200

    Ignore errors for missing linters in nvim

--- init.lua
+++ init.lua
@@ -244,7 +244,7 @@ require('lint').linters_by_ft = {
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
   callback = function()
-    require("lint").try_lint()
+    require("lint").try_lint(nil, { ignore_errors = true })
   end,
 })
 
